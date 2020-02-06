51e5e6c29b5fdd334ad12c4280862ca9b3a0cae2
zphoenixz <ing.ramiro.valdez.a@gmail.com>
2020-02-05 20:40:56 -0400

    CORS

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const app = express();
 
 // app.use(bodyParser.urlencoded());// x-www-form-urlenconded <form>
 app.use(bodyParser.json());// application/json
+//CORS HEADERS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 app.use('/feed', feedRoutes);
 
 
